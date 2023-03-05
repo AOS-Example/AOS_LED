3296c298e86a249b8ddb7194fdc7bbf72b88b2da
Dung Tran Anh <min98st1@gmail.com>
2023-03-05 21:19:29 +0700

    [HAL] Implement HAL client for LED
    Create hal client in the jni
    
    Signed-off-by: Dung Tran Anh <min98st1@gmail.com>

---
+++ c_exm/led_custom/interfaces/led/1.0/default/service.cpp
@@ -0,0 +1,35 @@
+#define LOG_TAG "demo.hardware.led@1.0-service"
+#include <log/log.h>
+#include <hidl/HidlSupport.h>
+#include <hidl/HidlTransportSupport.h>
+#include <demo/hardware/led/1.0/ILed.h>
+#include "Led.h"
+
+using demo::hardware::led::V1_0::implementation::Led;
+using android::sp;
+using android::status_t;
+
+int main(int /* argc */, char* /* argv */[]) {
+	/*
+	 * This function must be called before you join yo ensure
+	 * the proper number of threads are created. The threadpoll will
+	 * never exceed size one because of this call
+	 */
+	::android::hardware::configureRpcThreadpool(1 /*thread */, true /* willJoin */);
+
+	ALOGD("Prepare for demo.hardware.led@1.0-service");
+	sp led = new Led();
+	const status_t status = led->registerAsService();
+	if (status != ::android::OK) {
+		ALOGE("Can't register service of led");
+		return 1; // Or handle error
+	}
+
+	/*
+	 * Adds this thread to the threadpool, resulting in one total
+	 * thread in the threadpool. We could also do other thing, but
+	 * would have to specify false to willJoin in configureRpcThreadpool
+	 */
+	::android::hardware::joinRpcThreadpool();
+	return 1; // joinRpcThreadpool should never return
+}
---
+++ c_exm/led_custom/interfaces/led/1.0/hidl-generate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Stop execution instantly as a query exits while
+# having a non-zero status
+set -e
+
+if [ ! -d $ANDROID_BUILD_TOP/my_demo/interfaces ] ; then
+	echo "Where is my_demo/interfaces ?";
+	exit 1;
+fi
+
+if [ ! -d $ANDROID_BUILD_TOP/system/libhidl/transport ] ; then
+	echo "Where is system/libhidl/transport ?";
+	exit 1;
+fi
+
+PACKAGE=my_demo.hardware.led@1.0
+LOC=$ANDROID_BUILD_TOP/my_demo/interfaces/led/1.0/default/
+options="-rmy_demo.hardware:my_demo/interfaces \
+		-randroid.hidl:system/libhidl/transport"
+
+#make hidl-gen -j6
+hidl-gen -o $LOC -Lc++-impl $options $PACKAGE
+hidl-gen -o $LOC -Landroidbp
